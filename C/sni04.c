656f4a360f9df9279bd3ec0e9d892e495e3049dc
love-and-pinfu <you@example.com>
2019-06-17 01:33:02 +0900

    for文の追加

--- C/sni04.c
+++ C/sni04.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main(){
-    int a[5]={1,2,3,4,5},b;
-    b=a[0]+a[1]+a[2]+a[3]+a[4];
+    int i;
+    int a[5] = {1, 2, 3, 4, 5};
+    int b = 0;
+    for ( i = 0; i <= 4; i++)
+    {
+        b = b + a[i];
+    }
     printf("%d",b);
     return 0;
 }
